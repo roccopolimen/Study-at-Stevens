78fe4823299945ffe93259afaf4f792440869d55
Max Zaremba <max.zaremba@gmail.com>
2019-12-03 18:25:09 -0500

    updated js

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-var logfile;
-$.get("/classes/ma121/problems/Problem_1.txt", function(response) {
-    logfile = response;
-    alert(logfile);
-});
+//var file_txt = "";
+//    
+//"/classes/ma121/problems/Problem_5.txt"
+//
+////path = "/classes/"+target_class+"/problems/"+target_problem
+//$.get("/classes/"+target_class+"/problems/"+target_problem,  function(data) {
+//    file_txt = data;
+//    alert(file_txt);
+//});
+function load_problem(cl, num){
+    var logfile;
+    $.get("/classes/"+cl+"/problems/Problem_"+num+".txt", function(response) {
+        logfile = response.split("\n");
+        alert(response);
+
+        var topic = logfile[1];
+        
+        var prob_number = logfile[4];
+
+        var prob_body = "";
+        var n = 7;
+        while (logfile[n] != logfile[2]){
+            prob_body += logfile[n];
+            n++;
+        }
+
+        var solution = logfile[n+2];
+        
+        alert(topic + prob_number + prob_body + solution);
+    });
+}
