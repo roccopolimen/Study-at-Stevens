ee398f2f25bfe2db6e5d39edc1fd13ada601b3f0
Max Zaremba <max.zaremba@gmail.com>
2019-12-03 17:12:58 -0500

    JS stuff

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function load_problem(target_problem, target_class){
-    var txtFile = new XMLHttpRequest();
-    txtFile.open("GET", "/classes/"+target_class+"/problems/"+target_problem, true);
-    txtFile.onreadystatechange = function() {
-        if (txtFile.readyState === 4 && txtFile.status == 200) {
-            document.getElementById("problem_header").innerHTML = text;
-        }
+    file_txt = "";
+    
+    //path = "/classes/"+target_class+"/problems/"+target_problem
+    var client = new XMLHttpRequest();
+    client.open('GET', "/classes/"+target_class+"/problems/"+target_problem);
+    
+    client.onreadystatechange = function() {
+        alert(client.responseText);
     }
+    client.send();
 }
 
 window.onLoad = load_problem("Problem_1","ma121");
